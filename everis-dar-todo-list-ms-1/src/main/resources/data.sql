03f94e336e7a4ff3519c06a207471a95e3d71f89
Diegogm20 <49027945+Diegogm20@users.noreply.github.com>
2020-11-13 12:06:16 +0100

    Create data.sql
    Creamos el archivo data.sql para tener unos datos iniciales cuando corramos nuestra aplicación

---
+++ everis-dar-todo-list-ms-1/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Task VALUES (1,'Backend, microservicios con Spring boot.','Hecho');
+INSERT INTO Task VALUES (2,'Repositorio Proyecto, crear repositorio público de GitHub.','Hecho');
+INSERT INTO Task VALUES (3,'DevOps, desplegar imagen de la aplicación desde DockerHub en AKS.','En curso');
+INSERT INTO Task VALUES (4,'CRUD, contrato CRUD con Swagger editor.','Por hacer');
+INSERT INTO Task VALUES (5,'Frontend, con angular(typescript).','Por hacer.');
